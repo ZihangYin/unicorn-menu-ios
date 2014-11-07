660c633ac2f1b6c7381532b07eb75a75ac0cb4b6
Zihang Yin <yinzh009100@gmail.com>
2014-11-07 00:01:23 -0800

    Initial Commit

---
+++ unicorn-menu-ios-test1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  unicorn-menu-ios-test1
+//
+//  Created by Zihang Yin on 11/7/14.
+//  Copyright (c) 2014 Unicorn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
